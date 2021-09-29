520146132bd66dee8ed8e5ed94e55b2b882ab0aa
= <=>
2021-09-29 20:53:32 +0400

    I added send dialog test

--- test_api.py
+++ test_api.py
@@ -1,6 +1,12 @@
-from api import MainWindow
+from api import MainWindow, SendDialog
 
 
 def test_main_windo():
     mw = MainWindow()
     assert isinstance(mw, MainWindow)
+
+
+def test_send_dialog():
+    mw = MainWindow()
+    sd = SendDialog(mw)
+    assert isinstance(sd, SendDialog)
